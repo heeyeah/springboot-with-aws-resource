dbd4c0067a4dc387da3d680e443f61622b676675
heeye <gdshy221@gmail.com>
2024-09-04 14:50:47 +0900

    only secret manager

--- src/main/java/hee/aws/opensearch/OpenSearchConnectController.java
+++ src/main/java/hee/aws/opensearch/OpenSearchConnectController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +14,16 @@ public class OpenSearchConnectController {
 
     private final static Logger log = LoggerFactory.getLogger(OpenSearchConnectController.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final OpenSearchConnectService openSearchConnectService;
+//    private final JdbcTemplate jdbcTemplate;
 
     @GetMapping("/opensearch")
     public ResponseEntity<?> queryInOpenSearch(@RequestParam String message) {
 
-        String query = "SELECT * FROM opensearch_dashboards_sample_data_flights LIMIT 1";
-        String result = jdbcTemplate.queryForList(query).toString();
-        log.info("result={}", result);
+        openSearchConnectService.getSecret("heeye");
+//        String query = "SELECT * FROM opensearch_dashboards_sample_data_flights LIMIT 1";
+//        String result = jdbcTemplate.queryForList(query).toString();
+//        log.info("result={}", result);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/hee/aws/opensearch/OpenSearchConnectService.java
+++ src/main/java/hee/aws/opensearch/OpenSearchConnectService.java
@@ -1,30 +1,39 @@
-//package hee.aws.opensearch;
-//
-//import org.springframework.stereotype.Service;
-//import software.amazon.awssdk.regions.Region;
-//import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-//import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
-//import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
-//
-//@Service
-//public class OpenSearchConnectService {
-//
-//    private final SecretsManagerClient secretsClient;
-//
-//    public OpenSearchConnectService() {
-//        this.secretsClient = SecretsManagerClient.builder()
-//                .region(Region.of("ap-northeast-2")) // AWS Region 설정
-//                .build();
-//    }
-//
-//    public String getSecret(String secretName) {
-//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-//                .secretId(secretName)
-//                .build();
-//
-//        GetSecretValueResponse getSecretValueResponse = secretsClient.getSecretValue(getSecretValueRequest);
-//        return getSecretValueResponse.secretString();
-//    }
-//
-//    // Use
-//}
+package hee.aws.opensearch;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+@Service
+@RequiredArgsConstructor
+public class OpenSearchConnectService {
+
+    private final SecretsManagerClient secretsClient;
+    private final Logger log = LoggerFactory.getLogger(OpenSearchConnectService.class);
+
+    public OpenSearchConnectService() {
+        this.secretsClient = SecretsManagerClient.builder()
+                .region(Region.of("ap-northeast-2")) // AWS Region 설정
+                .build();
+    }
+
+    public String getSecret(String secretName) {
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse = secretsClient.getSecretValue(getSecretValueRequest);
+        String result = getSecretValueResponse.secretString();
+
+        log.info("result : {}", result);
+
+        return result;
+    }
+
+    // Use
+}
