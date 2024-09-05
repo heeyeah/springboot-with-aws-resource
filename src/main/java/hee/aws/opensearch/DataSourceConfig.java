c9b021577e196a8b06cdecb2b447ffbf63e1cc50
heeye <gdshy221@gmail.com>
2024-09-05 16:46:53 +0900

    culr

--- src/main/java/hee/aws/opensearch/DataSourceConfig.java
+++ src/main/java/hee/aws/opensearch/DataSourceConfig.java
@@ -62,7 +62,7 @@ public class DataSourceConfig {
 //        prop.setProperty("ssl", "true");
 //        prop.setProperty("auth", "NONE");
 
-        prop.put("auth", "BASIC");
+        prop.put("auth", "NONE");
 //        prop.put("auth", "AWS_SIGV4");
         prop.put("tunnelHost", "https://vpc-yr-vtr-kk66ndzelsd5nyucwx7h2dvkoi.ap-northeast-2.es.amazonaws.com");
 //        prop.put("awsCredentialsProvider", EnvironmentVariableCredentialsProvider.create());
